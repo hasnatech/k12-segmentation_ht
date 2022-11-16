77a9a1278954896630e27e9eb3041812d39ebd2c
Fashid <fashid@hasnatech.onmicrosoft.com>
2022-11-16 18:47:05 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as AOS from 'aos';
 import { MainService } from './service/main.service';
+import { security } from 'ht-security';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,9 @@ export class AppComponent implements AfterViewInit {
     AOS.init({
       once: true,
     });
+    security({
+      mode: "development"
+    })
     const script1 = document.createElement('script');
     script1.src = '//assets.adobedtm.com/47e3e446470b/b72afcffdbd7/launch-a14b516e7cd5.min.js';
     document.head.appendChild(script1);
